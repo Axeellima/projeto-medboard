9a9cb0017a7d37370beb86aa535c794f1ba41020
Tierry <tierryhahn@gmail.com>
2023-01-07 13:21:27 -0300

    feat/relationship

--- contact/models.py
+++ contact/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class Contact(models.Model):
     number = models.IntegerField(unique=True)
+
+    employee = models.OneToOneField("Employee", on_delete=models.CASCADE)
+    hospital = models.OneToOneField("Hospital", on_delete=models.CASCADE)
+    patient = models.OneToOneField("Patient", on_delete=models.CASCADE)
