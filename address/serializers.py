c9dc4725ece630dc4df445bc9716e686302797fd
Axel <axeellima@gmail.com>
2023-01-11 15:10:48 -0300

    fix: all conflicts of merge

--- address/serializers.py
+++ address/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
 
-class AddressSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    street = serializers.CharField(max_length=100)
-    number = serializers.IntegerField(unique=True)
-    district = serializers.CharField(max_length=50)
-    cep = serializers.IntegerField()
+from .models import Address
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = ["id", "street", "number", "district", "cep"]
