8978e3936f49e4b279769c2f96b789948ecd551c
Axel <axeellima@gmail.com>
2023-01-10 22:58:52 -0300

    add: consultation views

--- consultation/views.py
+++ consultation/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from .models import Consultation
+from .serializers import ConsultationSerializer
 
+from rest_framework.generics import ListCreateAPIView
+
+class ConsultationView(ListCreateAPIView):
+
+    queryset = Consultation.objects.all()
+    serializer_class = ConsultationSerializer
+
+    def perform_create(self, serializer):
+        
+        serializer.save(employee=self.request.user)
+
+        return serializer.data
